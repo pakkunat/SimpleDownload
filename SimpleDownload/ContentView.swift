9c85c0676b4f5d9c6a81cb42625788bfa99224b2
pakkun <pakkun_kh@fol.hi-ho.ne.jp>
2021-02-21 08:38:12 +0900

    .

--- SimpleDownload/ContentView.swift
+++ SimpleDownload/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  // for binding textfield
+  @State var status: String = ""
+  
+  var body: some View {
+    VStack {
+      // to act download
+      Button("Start Download", action: startDownload)
+      // to show download status
+      TextField("Status", text: $status)
     }
+  }
+  
+  func startDownload() {
+    // sample source
+    // https://developer.apple.com/documentation/foundation/url_loading_system/downloading_files_from_websites
+    
+    // weather data path of japan
+    let url = URL(string: "https://www.data.jma.go.jp/obd/stats/data/mdrr/snc_rct/alltable/snc00_rct.csv")!
+    // 1. create instance using singleton method
+    let downloadTask = URLSession.shared.downloadTask(with: url) { urlOrNil, responseOrNil, errorOrNil in
+      // 3. callback
+      // urlOrNil == nil, return
+      guard let fileURL = urlOrNil else { return }
+      print("fileURL.lastPathComponent: \(fileURL.lastPathComponent)")
+      do {
+        // get local documents directory
+        let documentsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        print("documentsURL.absoluteURL: \(documentsURL.absoluteURL)")
+        // get saved file path
+        let savedURL = documentsURL.appendingPathComponent(fileURL.lastPathComponent)
+        print("savedURL.absoluteURL: \(savedURL.absoluteURL)")
+        // move file
+        try FileManager.default.moveItem(at: fileURL, to: savedURL)
+        status = "succeed"
+      } catch {
+        status = "file error: \(error)"
+      }
+    }
+    // 2. execute download
+    downloadTask.resume()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
