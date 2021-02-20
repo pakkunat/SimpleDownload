2126d7f07764ffbfd9b34b17fcaecf348a5b3cb4
pakkun <pakkun_kh@fol.hi-ho.ne.jp>
2021-02-20 18:45:53 +0900

    Initial Commit

---
+++ SimpleDownload/SimpleDownloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDownloadApp.swift
+//  SimpleDownload
+//
+//  Created by pakkun on 2021/02/20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDownloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
